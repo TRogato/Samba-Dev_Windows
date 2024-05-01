bb0b2de0a68764aa267570652fdd6f91e6d5bc21
Tiago Rogato <70462857+TRogato@users.noreply.github.com>
2024-05-01 10:05:20 -0300

    Update script_samba.sh
    Correção criar usuario para compartilhamento

--- script_samba.sh
+++ script_samba.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-# Variables and Settings
-export SMB_USER="ubuntu"
-export SMB_PASS="123456"
+# Variáveis e Configurações
+export SMB_USER="seu_usuario"
+export SMB_PASS="sua_senha"
 
-# Install Samba
-echo "Installing Samba"
+# Instalar Samba
+echo "Instalando Samba"
 apt-get update > /dev/null
 apt-get install samba -y > /dev/null
 
-# Create directory for sharing
-mkdir -p /var/www
-chown $SMB_USER:root /var/www
+# Criar diretório para compartilhamento
+mkdir -p /var/www/html
+chown -R $SMB_USER:$SMB_USER /var/www/html
 
-# Configure
-echo "Configuring Samba"
+# Configurar
+echo "Configurando Samba"
 mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
 
 read -d '' SMB_CNFG <<"EOF"
@@ -22,8 +22,8 @@ read -d '' SMB_CNFG <<"EOF"
     workgroup = WORKGROUP
     security = user
 [www]
-    comment = Ubuntu File Server Share
-    path = /var/www
+    comment = Compartilhamento de Arquivos do Ubuntu
+    path = /var/www/html/
     browsable = yes
     guest ok = no
     read only = no
@@ -40,8 +40,11 @@ read -d '' SMB_CNFG <<"EOF"
 EOF
 
 echo "$SMB_CNFG" | tee /etc/samba/smb.conf > /dev/null
-printf '%s\n%s\n' "$SMB_PASS" "$SMB_PASS" | smbpasswd -sa -U $SMB_USER > /dev/null
 
-# Restart
-echo "Restarting Samba"
+# Criar usuário para o Samba
+useradd $SMB_USER -s /sbin/nologin
+echo -ne "$SMB_PASS\n$SMB_PASS\n" | smbpasswd -a -s $SMB_USER
+
+# Reiniciar
+echo "Reiniciando o Samba"
 service smbd restart
